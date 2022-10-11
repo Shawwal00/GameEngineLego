01c05f5d2ffd5ae0950c51ec7201e875d566ae72
Shawwal <shawwalrandom@gmail.com>
2022-10-11 14:25:53 +0100

    Working on the Cursor + Placing blocks

--- Assets/Scripts/PlaceObject.cs
+++ Assets/Scripts/PlaceObject.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlaceObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Theses are all public so that the user can change these to their own assets.
+    
+    // public GameObject wheel;
+    // public GameObject baseBlock;
+    // public GameObject engine;
+    // public GameObject fuel;
+    // public GameObject seat;
+    // public GameObject wings;
+    // public GameObject jet;
+
+    private GameObject cursor; 
+
+    private void Awake()
     {
+        //In the game I can check how many specific objects are in it by using the tags.
         
+        // wheel.gameObject.tag = "wheel";
+        // baseBlock.gameObject.tag = "baseBlock";
+        // engine.gameObject.tag = "engine";
+        // fuel.gameObject.tag = "fuel";
+        // seat.gameObject.tag = "seat";
+        // wings.gameObject.tag = "wings";
+        // jet.gameObject.tag = "jet";
+        
+        cursor = GameObject.Find("Cursor");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        moveCursor();
+    }
+
+    private void moveCursor()
+    {
+        Vector3 cursorPosition = cursor.transform.position;
+        //The below controls the cursor
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            cursorPosition.x =  10;
+            Debug.Log("GG");
+        }
     }
 }
